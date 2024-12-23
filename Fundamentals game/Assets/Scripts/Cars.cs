1c3c461a6e12b4f0e415bfd4c5bd4fb45dc4ed30
Ilse <570453@student.saxion.nl>
2024-12-23 13:15:29 +0100

    Moved prefabs do prefabs folder, added script for car border

---
+++ Fundamentals game/Assets/Scripts/Cars.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cars : MonoBehaviour
+{
+    public GameObject[] car1, car2, car3;
+    public GameObject carspawn1;
+    public GameObject carspawn2;
+    public GameObject carspawn3;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
